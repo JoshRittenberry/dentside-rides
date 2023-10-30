7fd9c17e7e534d80708557faf8eb060be51f5b71
JoshRittenberry <joshrittenberry96@gmail.com>
2023-10-30 10:39:14 -0500

    Saving

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,15 +1,17 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import "./Login.css"
 import { createUser, getUserByEmail } from "../../services/userService"
 
 export const Register = (props) => {
     const [customer, setCustomer] = useState({
+        username: "",
         email: "",
-        fullName: "",
-        isStaff: false,
-    })
-    let navigate = useNavigate()
+        password: "",
+        imageUrl: "",
+        joinDate: "",
+    });
+    let navigate = useNavigate();
 
     const registerNewUser = () => {
         createUser(customer).then((createdUser) => {
@@ -20,46 +22,58 @@ export const Register = (props) => {
                         id: createdUser.id,
                         staff: createdUser.isStaff,
                     })
-                )
+                );
 
-                navigate("/")
+                navigate("/");
             }
-        })
-    }
+        });
+    };
 
     const handleRegister = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         getUserByEmail(customer.email).then((response) => {
             if (response.length > 0) {
                 // Duplicate email. No good.
-                window.alert("Account with that email address already exists")
+                window.alert("Account with that email address already exists");
             } else {
                 // Good email, create user.
-                registerNewUser()
+                registerNewUser();
             }
-        })
-    }
+        });
+    };
 
     const updateCustomer = (evt) => {
-        const copy = { ...customer }
-        copy[evt.target.id] = evt.target.value
-        setCustomer(copy)
-    }
+        const copy = { ...customer };
+        copy[evt.target.id] = evt.target.value;
+        setCustomer(copy);
+    };
+
+    const getCurrentDate = () => {
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = (1 + date.getMonth()).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    };
+
+    useEffect(() => {
+        setCustomer(prevState => ({ ...prevState, joinDate: getCurrentDate() }));
+    }, []);
 
     return (
         <main style={{ textAlign: "center" }}>
             <img src="https://i.ibb.co/Z82H6NW/F100-1812-FGEN-42.png" className="login-logo" />
             <form className="form-login" onSubmit={handleRegister}>
-                <h1>Honey Rae Repairs</h1>
+                <h1>Dentside Rides</h1>
                 <h2>Please Register</h2>
                 <fieldset>
                     <div className="form-group">
                         <input
                             onChange={updateCustomer}
                             type="text"
-                            id="fullName"
+                            id="username"
                             className="form-control"
-                            placeholder="Enter your name"
+                            placeholder="Enter your Username"
                             required
                             autoFocus
                         />
@@ -72,31 +86,27 @@ export const Register = (props) => {
                             type="email"
                             id="email"
                             className="form-control"
-                            placeholder="Email address"
+                            placeholder="Email Address"
                             required
                         />
                     </div>
                 </fieldset>
                 <fieldset>
                     <div className="form-group">
-                        <label>
-                            <input
-                                onChange={(evt) => {
-                                    const copy = { ...customer }
-                                    copy.isStaff = evt.target.checked
-                                    setCustomer(copy)
-                                }}
-                                type="checkbox"
-                                id="isStaff"
-                            />
-                            I am an admin{" "}
-                        </label>
+                        <input
+                            onChange={updateCustomer}
+                            type="url"
+                            id="imageUrl"
+                            className="form-control"
+                            placeholder="Image URL"
+                            required
+                        />
                     </div>
                 </fieldset>
                 <fieldset>
                     <div className="form-group">
                         <button className="login-btn btn-info" type="submit">
-                            Register
+                            Create Account
                         </button>
                     </div>
                 </fieldset>
---
+++ src/components/posts/DeletePostBTN.js
@@ -0,0 +1,7 @@
+export const DeletePostBTN = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
