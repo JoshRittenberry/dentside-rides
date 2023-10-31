ec4176dbdd8589a79c8921faff167bb58d504849
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2023-10-31 09:33:55 -0500

    Added code to delete local storage on page exit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import { ApplicationViews } from "./views/ApplicationViews"
 import { Login } from "./components/auth/Login"
 import { Register } from "./components/auth/Register"
 import { Authorized } from "./views/Authorized"
+import { useEffect } from "react"
 
 export const App = () => {
+  useEffect(() => {
+    function handleUnload() {
+      localStorage.removeItem('dentside_user');
+    }
+
+    window.addEventListener("beforeunload", handleUnload);
+
+    // Cleanup event listener on unmount
+    return () => {
+      window.removeEventListener("beforeunload", handleUnload);
+    };
+  }, []);
+
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
